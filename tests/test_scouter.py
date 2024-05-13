c470244d87a5da44b7e8d62dceec51d044cf05c1
Thorrester <steven.forrester@shipt.com>
2024-05-13 16:46:10 -0400

    test coverage

--- tests/test_scouter.py
+++ tests/test_scouter.py
@@ -50,3 +50,25 @@ def test_pandas(array: NDArray):
     assert profile.features["0"].center == pytest.approx(1.5, 0.1)
     assert profile.features["1"].center == pytest.approx(2.5, 0.1)
     assert profile.features["2"].center == pytest.approx(3.5, 0.1)
+
+
+def test_fail(array: NDArray):
+    scouter = Scouter()
+    with pytest.raises(ValueError):
+        scouter.create_monitoring_profile(data="fail")
+
+    with pytest.raises(ValueError):
+        scouter.create_monitoring_profile(array.astype("str"))
+
+
+def test_int(array: NDArray):
+    # convert to int32
+    array = array.astype("int32")
+
+    scouter = Scouter()
+    profile: MonitorProfile = scouter.create_monitoring_profile(array)
+
+    # assert features are relatively centered
+    assert profile.features["feature_0"].center == pytest.approx(1.0, 0.1)
+    assert profile.features["feature_1"].center == pytest.approx(2.0, 0.1)
+    assert profile.features["feature_2"].center == pytest.approx(3.0, 0.1)
