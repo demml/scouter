5e496a91c96dda12d72404b725f4a9c656697cf1
Thorrester <sjforrester32@gmail.com>
2024-08-02 16:45:45 -0400

    format

--- py-scouter/python/scouter/scouter.py
+++ py-scouter/python/scouter/scouter.py
@@ -70,9 +70,7 @@ class Profiler:
                 bin_size=bin_size,
             )
 
-            assert isinstance(
-                profile, DataProfile
-            ), f"Expected DataProfile, got {type(profile)}"
+            assert isinstance(profile, DataProfile), f"Expected DataProfile, got {type(profile)}"
             return profile
 
         except Exception as exc:  # type: ignore
@@ -126,9 +124,7 @@ class Drifter:
                 monitor_config.update_feature_map(string_profile.config.feature_map)
 
             if array.numeric_array is not None and array.numeric_features is not None:
-                numeric_profile = getattr(
-                    self._drifter, f"create_numeric_drift_profile_f{bits}"
-                )(
+                numeric_profile = getattr(self._drifter, f"create_numeric_drift_profile_f{bits}")(
                     features=array.numeric_features,
                     array=array.numeric_array,
                     monitor_config=monitor_config,
@@ -174,21 +170,14 @@ class Drifter:
             bits = _get_bits(array.numeric_array)
 
             if array.string_array is not None and array.string_features is not None:
-                string_array: NDArray = getattr(
-                    self._drifter, f"convert_strings_to_numpy_f{bits}"
-                )(
+                string_array: NDArray = getattr(self._drifter, f"convert_strings_to_numpy_f{bits}")(
                     array=array.string_array,
                     features=array.string_features,
                     drift_profile=drift_profile,
                 )
 
-                if (
-                    array.numeric_array is not None
-                    and array.numeric_features is not None
-                ):
-                    array.numeric_array = np.concatenate(
-                        (array.numeric_array, string_array), axis=1
-                    )
+                if array.numeric_array is not None and array.numeric_features is not None:
+                    array.numeric_array = np.concatenate((array.numeric_array, string_array), axis=1)
 
                     array.numeric_features += array.string_features
 
@@ -202,9 +191,7 @@ class Drifter:
                 drift_profile=drift_profile,
             )
 
-            assert isinstance(
-                drift_map, DriftMap
-            ), f"Expected DriftMap, got {type(drift_map)}"
+            assert isinstance(drift_map, DriftMap), f"Expected DriftMap, got {type(drift_map)}"
 
             return drift_map
 
@@ -258,9 +245,7 @@ class MonitorQueue:
         self._monitor = ScouterDrifter()
         self._drift_profile = drift_profile
 
-        self.feature_queue: Dict[str, List[float]] = {
-            feature: [] for feature in self.feature_names
-        }
+        self.feature_queue: Dict[str, List[float]] = {feature: [] for feature in self.feature_names}
         self._count = 0
 
         self._producer = self._get_producer(config)
@@ -306,9 +291,7 @@ class MonitorQueue:
             data = list(self.feature_queue.values())
             array = np.array(data, dtype=np.float64).T
 
-            drift_records = self._monitor.sample_data_f64(
-                self.feature_names, array, self._drift_profile
-            )
+            drift_records = self._monitor.sample_data_f64(self.feature_names, array, self._drift_profile)
 
             for record in drift_records:
                 self._producer.publish(record)
