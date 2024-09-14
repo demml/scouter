5bb99aaf6d330ee9c83bad106d2a2bc2e7fba1ae
Thorrester <sjforrester32@gmail.com>
2024-09-14 10:04:44 -0400

    revert back to ndarray stats

--- crates/scouter/src/core/stats.rs
+++ crates/scouter/src/core/stats.rs
@@ -1,64 +1,36 @@
 use ndarray::prelude::*;
-use rayon::prelude::*;
-use std::{collections::BTreeMap, sync::Arc};
+use ndarray_stats::CorrelationExt;
+use std::collections::BTreeMap;
 
-fn compute_correlation(x: &ArrayView1<f64>, y: &ArrayView1<f64>) -> f64 {
-    if x.len() != y.len() {
-        panic!("Arrays must have the same length");
-    }
-
-    let x_mean = x.mean().unwrap();
-    let y_mean = y.mean().unwrap();
-
-    let numerator = x
-        .iter()
-        .zip(y.iter())
-        .map(|(&xi, &yi)| (xi - x_mean) * (yi - y_mean))
-        .sum::<f64>();
-
-    let x_variance = x.iter().map(|&xi| (xi - x_mean).powi(2)).sum::<f64>();
-
-    let y_variance = y.iter().map(|&yi| (yi - y_mean).powi(2)).sum::<f64>();
-
-    numerator / (x_variance.sqrt() * y_variance.sqrt())
-}
-
+// compute_feature_correlations computes the correlation between features in a 2D array
+//
+// # Arguments
+//
+// * `data` - A 2D array of data
+// * `features` - A vector of feature names
+//
+// # Returns
+//
+// A BTreeMap of feature names to a BTreeMap of other feature names to the correlation value
 pub fn compute_feature_correlations(
     data: &ArrayView2<f64>,
     features: &[String],
 ) -> BTreeMap<String, BTreeMap<String, f64>> {
-    let data_arc = Arc::new(data.clone()); // Clone once, then use Arc for cheap cloning
     let mut feature_correlations: BTreeMap<String, BTreeMap<String, f64>> = BTreeMap::new();
+    let correlations = data.t().pearson_correlation().unwrap();
 
-    let correlations: Vec<Vec<f64>> = data
-        .axis_iter(Axis(1))
-        .into_par_iter()
-        .enumerate()
-        .map(|(i, column)| {
-            let data = Arc::clone(&data_arc);
-            data.axis_iter(Axis(1))
-                .into_par_iter()
-                .enumerate()
-                .map(|(j, other_column)| {
-                    if i == j {
-                        1.0
-                    } else {
-                        compute_correlation(&column, &other_column)
-                    }
-                })
-                .collect::<Vec<f64>>()
-        })
-        .collect();
-
-    for (i, feature) in features.iter().enumerate() {
+    features.iter().enumerate().for_each(|(i, feature)| {
         let mut feature_correlation: BTreeMap<String, f64> = BTreeMap::new();
-        for (j, other_feature) in features.iter().enumerate() {
+        features.iter().enumerate().for_each(|(j, other_feature)| {
             if i != j {
-                feature_correlation.insert(other_feature.clone(), correlations[i][j]);
+                let value = correlations[[i, j]];
+                // extract the correlation value
+                feature_correlation.insert(other_feature.clone(), value);
             }
-        }
+        });
         feature_correlations.insert(feature.clone(), feature_correlation);
-    }
+    });
+
     feature_correlations
 }
 
@@ -84,45 +56,30 @@ mod tests {
     }
 
     #[test]
-    fn test_correlation_1d() {
-        let (x, y) = generate_correlated_arrays(1000, 0.75);
-        let computed_correlation = compute_correlation(&x.view(), &y.view());
-        let expected_correlation = 0.75;
-        assert!((computed_correlation - expected_correlation).abs() < 0.1);
-
-        let (x, y) = generate_correlated_arrays(1000, 0.33);
-        let computed_correlation = compute_correlation(&x.view(), &y.view());
-        let expected_correlation = 0.33;
-        assert!((computed_correlation - expected_correlation).abs() < 0.1);
-    }
-
-    #[test]
-    fn test_negative_correlation_1d() {
-        let x = array![1., 2., 3., 4., 5.];
-        let y = array![5., 4., 3., 2., 1.];
-        assert!((compute_correlation(&x.view(), &y.view()) + 1.0).abs() < 1e-10);
-    }
-
-    #[test]
     fn test_correlation_2d() {
         // generate first set
-        let (x1, y1) = generate_correlated_arrays(10000, 0.75);
+        let (x1, y1) = generate_correlated_arrays(20000, 0.75);
 
         // generate second set
-        let (x2, y2) = generate_correlated_arrays(10000, 0.33);
+        let (x2, y2) = generate_correlated_arrays(20000, 0.33);
+
+        let (x3, y3) = generate_correlated_arrays(20000, -0.80);
 
         // combine into 4 columns
-        let data = stack![Axis(1), x1, y1, x2, y2];
+        let data = stack![Axis(1), x1, y1, x2, y2, x3, y3];
         let features = vec![
             "x1".to_string(),
             "y1".to_string(),
             "x2".to_string(),
             "y2".to_string(),
+            "x3".to_string(),
+            "y3".to_string(),
         ];
 
         let correlations = compute_feature_correlations(&data.view(), &features);
 
         assert!((correlations["x1"]["y1"] - 0.75).abs() < 0.1);
         assert!((correlations["x2"]["y2"] - 0.33).abs() < 0.1);
+        assert!((correlations["x3"]["y3"] + 0.80).abs() < 0.1);
     }
 }
