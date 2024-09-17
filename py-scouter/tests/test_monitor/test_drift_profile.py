98b815d280ac28424a849324205162df60d43a71
Thorrester <sjforrester32@gmail.com>
2024-09-17 17:34:42 -0400

    Create test_drift_profile.py

---
+++ py-scouter/tests/test_monitor/test_drift_profile.py
@@ -0,0 +1,24 @@
+from scouter import Drifter
+
+from numpy.typing import NDArray
+import pytest
+
+from scouter._scouter import (
+    DriftProfile,
+    DriftConfig,
+)
+
+
+def test_drift_profile_methods(array: NDArray, drift_config: DriftConfig):
+    drifter = Drifter()
+    profile: DriftProfile = drifter.create_drift_profile(array, drift_config)
+
+    profile_dict = profile.model_dump()
+
+    assert isinstance(profile_dict, dict)
+    assert profile_dict["features"]["feature_0"]["center"] == pytest.approx(1.5, 0.1)
+
+    new_profile = DriftProfile.model_validate(profile_dict)
+
+    # check if the new profile is the same as the original
+    assert new_profile.model_dump() == profile_dict
