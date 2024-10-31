12dbd6981bd147006396dc73c26017ca56f18768
Thorrester <sjforrester32@gmail.com>
2024-10-31 09:57:02 -0400

    Update test_psi_drift.py

--- py-scouter/tests/test_drift/test_psi_drift.py
+++ py-scouter/tests/test_drift/test_psi_drift.py
@@ -5,32 +5,37 @@ import pandas as pd
 from numpy.typing import NDArray
 import pytest
 import polars as pl
-from scouter._scouter import PsiDriftProfile, PsiDriftConfig
+from scouter._scouter import PsiDriftProfile, PsiDriftConfig, DriftType
 from tempfile import TemporaryDirectory
 from scouter.utils.types import Constants
 
 
-def test_drift_f64(array: NDArray, drift_config: SpcDriftConfig):
-    drifter = Drifter()
-    profile: SpcDriftProfile = drifter.create_drift_profile(array, drift_config)
+def test_drift_f64(array: NDArray, psi_drift_config: PsiDriftConfig):
+    drifter = Drifter(DriftType.PSI)
+    profile: PsiDriftProfile = drifter.create_drift_profile(array, psi_drift_config)
+
+    print(profile)
+    a
 
     # assert features are relatively centered
-    assert profile.features["feature_0"].center == pytest.approx(1.5, 0.1)
-    assert profile.features["feature_1"].center == pytest.approx(2.5, 0.1)
-    assert profile.features["feature_2"].center == pytest.approx(3.5, 0.1)
 
-    # save profile to json
-    with TemporaryDirectory() as temp_dir:
-        profile.save_to_json(Path(temp_dir) / "profile.json")
-        assert (Path(temp_dir) / "profile.json").exists()
 
-        # test loading from json file
-        with open(Path(temp_dir) / "profile.json", "r") as f:
-            SpcDriftProfile.model_validate_json(f.read())
+# assert profile.features["feature_0"].center == pytest.approx(1.5, 0.1)
+# assert profile.features["feature_1"].center == pytest.approx(2.5, 0.1)
+# assert profile.features["feature_2"].center == pytest.approx(3.5, 0.1)
 
-    _ = drifter.compute_drift(array, profile)
+## save profile to json
+# with TemporaryDirectory() as temp_dir:
+#    profile.save_to_json(Path(temp_dir) / "profile.json")
+#    assert (Path(temp_dir) / "profile.json").exists()
 
-    profile.update_config_args(repository="repo1", name="name1")
+#    # test loading from json file
+#    with open(Path(temp_dir) / "profile.json", "r") as f:
+#        SpcDriftProfile.model_validate_json(f.read())
 
-    assert profile.config.name == "name1"
-    assert profile.config.repository == "repo1"
+# _ = drifter.compute_drift(array, profile)
+
+# profile.update_config_args(repository="repo1", name="name1")
+
+# assert profile.config.name == "name1"
+# assert profile.config.repository == "repo1"
