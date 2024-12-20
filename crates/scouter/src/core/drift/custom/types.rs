3b1f60eadf1d04011bb74f8a3c375fd0d4b39ace
Russell Kemmit <russell.kemmit@shipt.com>
2024-12-20 12:12:59 -0600

    add custom metric type tests

--- crates/scouter/src/core/drift/custom/types.rs
+++ crates/scouter/src/core/drift/custom/types.rs
@@ -443,30 +443,30 @@ impl ComparisonMetricAlert {
     fn alert_description_header(&self) -> String {
         let below_threshold = |boundary: Option<f64>| match boundary {
             Some(b) => format!(
-                "The metric value has dropped below the threshold (initial value - {}) for {}",
-                b, self.metric_name
+                "The observed {} metric value has dropped below the threshold (initial value - {})",
+                self.metric_name, b
             ),
             None => format!(
-                "The metric value has dropped below the initial value for {}",
+                "The {} metric value has dropped below the initial value",
                 self.metric_name
             ),
         };
 
         let above_threshold = |boundary: Option<f64>| match boundary {
             Some(b) => format!(
-                "The metric value has increased beyond the threshold (initial value + {}) for {}",
-                b, self.metric_name
+                "The {} metric value has increased beyond the threshold (initial value + {})",
+                self.metric_name, b
             ),
             None => format!(
-                "The metric value has increased beyond the initial value for {}",
+                "The {} metric value has increased beyond the initial value",
                 self.metric_name
             ),
         };
 
         let outside_threshold = |boundary: Option<f64>| match boundary {
             Some(b) => format!(
-                "The metric value has fallen outside the threshold (initial value ± {}) for {}",
-                b, self.metric_name
+                "The {} metric value has fallen outside the threshold (initial value ± {})",
+                self.metric_name, b,
             ),
             None => format!(
                 "The metric value has fallen outside the initial value for {}",
@@ -483,25 +483,17 @@ impl ComparisonMetricAlert {
 }
 
 impl DispatchAlertDescription for ComparisonMetricAlert {
-    fn create_alert_description(&self, dispatch_type: AlertDispatchType) -> String {
+    // TODO make pretty per dispatch type
+    fn create_alert_description(&self, _dispatch_type: AlertDispatchType) -> String {
         let mut alert_description = String::new();
         let header = format!("{}\n", self.alert_description_header());
         alert_description.push_str(&header);
 
         let current_metric = format!("Current Metric Value: {}\n", self.observed_metric_value);
-        let historical_metric =
-            format!("Historical Metric Value: {}\n", self.training_metric_value);
+        let historical_metric = format!("Initial Metric Value: {}\n", self.training_metric_value);
 
-        let feature_name = match dispatch_type {
-            AlertDispatchType::Console | AlertDispatchType::OpsGenie => {
-                format!("{:indent$}{}: \n", "", &self.metric_name, indent = 4)
-            }
-            AlertDispatchType::Slack => format!("{}: \n", &self.metric_name),
-        };
-
-        alert_description.push_str(&feature_name);
-        alert_description.push_str(&current_metric);
         alert_description.push_str(&historical_metric);
+        alert_description.push_str(&current_metric);
 
         alert_description
     }
@@ -568,5 +560,188 @@ impl CustomMetricServerRecord {
         record.insert("metric".to_string(), self.metric.clone());
         record.insert("value".to_string(), self.value.to_string());
         record
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alert_config() {
+        //test console alert config
+        let dispatch_type = AlertDispatchType::OpsGenie;
+        let schedule = "0 0 * * * *".to_string();
+        let mut alert_config =
+            CustomMetricAlertConfig::new(Some(dispatch_type), Some(schedule), None);
+        assert_eq!(alert_config.dispatch_type(), "OpsGenie");
+
+        let custom_metrics = vec![
+            CustomMetric::new("mae".to_string(), 12.4, AlertCondition::ABOVE, Some(2.3)),
+            CustomMetric::new("accuracy".to_string(), 0.85, AlertCondition::BELOW, None),
+        ];
+
+        alert_config.set_alert_conditions(&custom_metrics);
+
+        if let Some(alert_conditions) = alert_config.alert_conditions.as_ref() {
+            assert_eq!(
+                alert_conditions["mae"].alert_condition,
+                AlertCondition::ABOVE
+            );
+            assert_eq!(alert_conditions["mae"].alert_boundary, Some(2.3));
+            assert_eq!(
+                alert_conditions["accuracy"].alert_condition,
+                AlertCondition::BELOW
+            );
+            assert_eq!(alert_conditions["accuracy"].alert_boundary, None);
+        } else {
+            panic!("alert_conditions should not be None");
+        }
+    }
+
+    #[test]
+    fn test_drift_config() {
+        let mut drift_config = CustomMetricDriftConfig::new(None, None, None, None, None).unwrap();
+        assert_eq!(drift_config.name, "__missing__");
+        assert_eq!(drift_config.repository, "__missing__");
+        assert_eq!(drift_config.version, "0.1.0");
+        assert_eq!(
+            drift_config.alert_config.dispatch_type,
+            AlertDispatchType::Console
+        );
+
+        let new_alert_config = CustomMetricAlertConfig::new(
+            Some(AlertDispatchType::Slack),
+            Some("0 0 * * * *".to_string()),
+            None,
+        );
+
+        // update
+        drift_config
+            .update_config_args(None, Some("test".to_string()), None, Some(new_alert_config))
+            .unwrap();
+
+        assert_eq!(drift_config.name, "test");
+        assert_eq!(
+            drift_config.alert_config.dispatch_type,
+            AlertDispatchType::Slack
+        );
+        assert_eq!(
+            drift_config.alert_config.schedule,
+            "0 0 * * * *".to_string()
+        );
+    }
+
+    #[test]
+    fn test_custom_drift_profile() {
+        let alert_config = CustomMetricAlertConfig::new(
+            Some(AlertDispatchType::OpsGenie),
+            Some("0 0 * * * *".to_string()),
+            None,
+        );
+        let drift_config = CustomMetricDriftConfig::new(
+            Some("scouter".to_string()),
+            Some("ML".to_string()),
+            Some("0.1.0".to_string()),
+            Some(alert_config),
+            None,
+        )
+        .unwrap();
+
+        let custom_metrics = vec![
+            CustomMetric::new("mae".to_string(), 12.4, AlertCondition::ABOVE, Some(2.3)),
+            CustomMetric::new("accuracy".to_string(), 0.85, AlertCondition::BELOW, None),
+        ];
+
+        let profile = CustomDriftProfile::new(drift_config, custom_metrics, None).unwrap();
+
+        let expected_profile = r#"{
+        "config": {
+                    "repository": "scouter",
+                    "name": "ML",
+                    "version": "0.1.0",
+                    "alert_config": {
+                        "dispatch_type": "OpsGenie",
+                        "schedule": "0 0 * * * *",
+                        "dispatch_kwargs": {},
+                        "alert_conditions": {
+                            "mae": {
+                                "alert_condition": "ABOVE",
+                                "alert_boundary": 2.3
+                            },
+                            "accuracy": {
+                                "alert_condition": "BELOW",
+                                "alert_boundary": null
+                            }
+                        }
+                    },
+                    "drift_type": "CUSTOM"
+                },
+                "metrics": {
+                    "mae": 12.4,
+                    "accuracy": 0.85
+                },
+                "scouter_version": "0.3.3"
+            }"#;
+
+        let profile_json: Value =
+            serde_json::from_str(&profile.model_dump_json()).expect("Failed to parse actual JSON");
+        let expected_profile_json: Value =
+            serde_json::from_str(expected_profile).expect("Failed to parse expected JSON");
+
+        assert_eq!(
+            profile_json, expected_profile_json,
+            "The JSON structures do not match!"
+        );
+    }
+
+    #[test]
+    fn test_create_alert_description() {
+        let alert_above_threshold = ComparisonMetricAlert {
+            metric_name: "mse".to_string(),
+            training_metric_value: 12.5,
+            observed_metric_value: 14.0,
+            alert_boundary: Some(1.0),
+            alert_condition: AlertCondition::ABOVE,
+        };
+
+        let description =
+            alert_above_threshold.create_alert_description(AlertDispatchType::Console);
+        assert!(description.contains(
+            "The mse metric value has increased beyond the threshold (initial value + 1)"
+        ));
+        assert!(description.contains("Initial Metric Value: 12.5"));
+        assert!(description.contains("Current Metric Value: 14"));
+
+        let alert_below_threshold = ComparisonMetricAlert {
+            metric_name: "accuracy".to_string(),
+            training_metric_value: 0.9,
+            observed_metric_value: 0.7,
+            alert_boundary: None,
+            alert_condition: AlertCondition::BELOW,
+        };
+
+        let description =
+            alert_below_threshold.create_alert_description(AlertDispatchType::Console);
+        assert!(
+            description.contains("The accuracy metric value has dropped below the initial value")
+        );
+        assert!(description.contains("Initial Metric Value: 0.9"));
+        assert!(description.contains("Current Metric Value: 0.7"));
+
+        let alert_outside_threshold = ComparisonMetricAlert {
+            metric_name: "mae".to_string(),
+            training_metric_value: 12.5,
+            observed_metric_value: 22.0,
+            alert_boundary: Some(2.0),
+            alert_condition: AlertCondition::OUTSIDE,
+        };
+
+        let description =
+            alert_outside_threshold.create_alert_description(AlertDispatchType::Console);
+        assert!(description
+            .contains("The mae metric value has fallen outside the threshold (initial value ± 2)"));
+        assert!(description.contains("Initial Metric Value: 12.5"));
+        assert!(description.contains("Current Metric Value: 22"));
     }
 }
