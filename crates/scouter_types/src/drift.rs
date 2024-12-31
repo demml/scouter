ca8a4d2d0bf46503255790d9554233fd32fa2838
Thorrester <sjforrester32@gmail.com>
2024-12-31 14:14:50 -0500

    scouter error

--- crates/scouter_types/src/drift.rs
+++ crates/scouter_types/src/drift.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use scouter_error::ScouterError;
 
 #[pyclass(eq)]
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
