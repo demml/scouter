ff95644fda589ddc10d9673cda257c1fc05536a3
Thorrester <sjforrester32@gmail.com>
2025-01-01 09:57:03 -0500

    setup

--- crates/scouter_server/src/api/setup.rs
+++ crates/scouter_server/src/api/setup.rs
@@ -1,6 +1,5 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
 use std::io;
-use tracing::{error, info};
 
 use tracing_subscriber;
 use tracing_subscriber::fmt::time::UtcTime;
@@ -24,34 +23,3 @@ pub async fn setup_logging() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-/// Setup the application with the given database pool.
-pub async fn create_db_pool(database_url: Option<String>) -> Result<Pool<Postgres>, anyhow::Error> {
-    // get env var
-    let database_url = database_url.unwrap_or_else(|| {
-        std::env::var("DATABASE_URL")
-            .unwrap_or("postgresql://postgres:admin@localhost:5432/scouter?".to_string())
-    });
-
-    // get max connections from env or set to 10
-    let max_connections = std::env::var("MAX_CONNECTIONS")
-        .unwrap_or_else(|_| "10".to_string())
-        .parse::<u32>()
-        .expect("MAX_CONNECTIONS must be a number");
-
-    let pool = match PgPoolOptions::new()
-        .max_connections(max_connections)
-        .connect(&database_url)
-        .await
-    {
-        Ok(pool) => {
-            info!("✅ Successfully connected to database");
-            pool
-        }
-        Err(err) => {
-            error!("🚨 Failed to connect to database {:?}", err);
-            std::process::exit(1);
-        }
-    };
-
-    Ok(pool)
-}
