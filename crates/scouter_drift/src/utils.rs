8a00b10a6cdf2f5827c5aea0f0b82a4a97a1e500
Thorrester <sjforrester32@gmail.com>
2025-01-01 13:25:35 -0500

    Update utils.rs

--- crates/scouter_drift/src/utils.rs
+++ crates/scouter_drift/src/utils.rs
@@ -1,7 +1,6 @@
 use scouter_error::MonitorError;
 use scouter_types::FeatureMap;
 use ndarray::{Array, Array2};
-use pyo3::prelude::*;
 use rayon::iter::IndexedParallelIterator;
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
@@ -138,5 +137,91 @@ where {
             .map_err(|e| MonitorError::ArrayError(e.to_string()))?;
 
         Ok(data.t().to_owned())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use crate::utils::CategoricalFeatureHelpers;
+
+
+    pub struct TestStruct;
+    impl CategoricalFeatureHelpers for TestStruct {}
+
+    
+    #[test]
+    fn test_create_feature_map_base() {
+        let string_vec = vec![
+            vec![
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+                "d".to_string(),
+                "e".to_string(),
+            ],
+            vec![
+                "hello".to_string(),
+                "blah".to_string(),
+                "c".to_string(),
+                "d".to_string(),
+                "e".to_string(),
+                "hello".to_string(),
+                "blah".to_string(),
+                "c".to_string(),
+                "d".to_string(),
+                "e".to_string(),
+            ],
+        ];
+
+        let string_features = vec!["feature_1".to_string(), "feature_2".to_string()];
+
+        let feature_map = TestStruct
+            .create_feature_map(&string_features, &string_vec)
+            .unwrap();
+
+        assert_eq!(feature_map.features.len(), 2);
+        assert_eq!(feature_map.features.get("feature_2").unwrap().len(), 6);
+    }
+
+    #[test]
+    fn test_create_array_from_string() {
+        let string_vec = vec![
+            vec![
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+                "d".to_string(),
+                "e".to_string(),
+            ],
+            vec![
+                "a".to_string(),
+                "a".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+                "b".to_string(),
+            ],
+        ];
+
+        let string_features = vec!["feature_1".to_string(), "feature_2".to_string()];
+
+        let feature_map = TestStruct
+            .create_feature_map(&string_features, &string_vec)
+            .unwrap();
+
+        assert_eq!(feature_map.features.len(), 2);
+
+        let f32_array = TestStruct
+            .convert_strings_to_ndarray_f32(&string_features, &string_vec, &feature_map)
+            .unwrap();
+
+        assert_eq!(f32_array.shape(), &[5, 2]);
+
+        let f64_array = TestStruct
+            .convert_strings_to_ndarray_f64(&string_features, &string_vec, &feature_map)
+            .unwrap();
+
+        assert_eq!(f64_array.shape(), &[5, 2]);
     }
 }
