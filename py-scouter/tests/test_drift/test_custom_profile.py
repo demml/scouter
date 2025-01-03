73586db8bfd6793415828e1b3e0ff8ba93406199
Thorrester <sjforrester32@gmail.com>
2025-01-03 16:17:04 -0500

    fixing for changes in scouter_version

--- py-scouter/tests/test_drift/test_custom_profile.py
+++ py-scouter/tests/test_drift/test_custom_profile.py
@@ -23,22 +23,20 @@ def test_custom_profile(custom_metric_drift_config: CustomMetricDriftConfig):
     # create custom drift profile
     profile: CustomDriftProfile = drifter.create_drift_profile(data=accuracy, config=custom_metric_drift_config)
     # assert profile is what we expect
-    assert profile.model_dump() == {
-        "config": {
-            "alert_config": {
-                "alert_conditions": {"accuracy": {"alert_threshold": "Below", "alert_threshold_value": 0.05}},
-                "dispatch_kwargs": {},
-                "dispatch_type": "Slack",
-                "schedule": "0 0 * * * *",
-            },
-            "drift_type": "Custom",
-            "name": "test",
-            "repository": "test",
-            "version": "0.1.0",
+    assert profile.model_dump()["config"] == {
+        "alert_config": {
+            "alert_conditions": {"accuracy": {"alert_threshold": "Below", "alert_threshold_value": 0.05}},
+            "dispatch_kwargs": {},
+            "dispatch_type": "Slack",
+            "schedule": "0 0 * * * *",
         },
-        "metrics": {"accuracy": 0.75},
-        "scouter_version": "0.3.3",
+        "drift_type": "Custom",
+        "name": "test",
+        "repository": "test",
+        "version": "0.1.0",
     }
+
+    assert profile.model_dump()["metrics"] == {"accuracy": 0.75}
 
     # test helper function that allows users to see their metrics in the format they were submitted
     assert profile.custom_metrics[0].name == "accuracy"
