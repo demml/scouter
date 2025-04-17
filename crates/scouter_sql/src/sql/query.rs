5ce21fbc1ba88742373b67dbc594ca2464ded2fc
Thorrester <sjforrester32@gmail.com>
2025-04-17 11:27:33 -0400

    organizing queries

--- crates/scouter_sql/src/sql/query.rs
+++ crates/scouter_sql/src/sql/query.rs
@@ -1,32 +1,57 @@
 //constants
 
-const INSERT_DRIFT_RECORD: &str = include_str!("scripts/insert_drift_record.sql");
-const INSERT_BIN_COUNTS: &str = include_str!("scripts/insert_bin_counts.sql");
-const GET_SPC_FEATURES: &str = include_str!("scripts/unique_spc_features.sql");
-const GET_BINNED_SPC_FEATURE_VALUES: &str = include_str!("scripts/binned_spc_feature_values.sql");
+// spc
+const INSERT_DRIFT_RECORD: &str = include_str!("scripts/spc/insert_spc_drift_record.sql");
+const GET_SPC_FEATURES: &str = include_str!("scripts/spc/unique_spc_features.sql");
+const GET_BINNED_SPC_FEATURE_VALUES: &str =
+    include_str!("scripts/spc/binned_spc_feature_values.sql");
+const GET_SPC_FEATURE_VALUES: &str = include_str!("scripts/spc/get_spc_feature_values.sql");
+const GET_SPC_ENTITIES: &str = include_str!("scripts/spc/get_spc_entities_for_archive.sql");
+const GET_SPC_DATA_FOR_ARCHIVE: &str = include_str!("scripts/spc/get_spc_data_for_archive.sql");
+
+// psi
+const INSERT_BIN_COUNTS: &str = include_str!("scripts/psi/insert_bin_counts.sql");
 const GET_BINNED_PSI_FEATURE_BINS: &str =
-    include_str!("scripts/binned_psi_feature_bin_proportions.sql");
+    include_str!("scripts/psi/binned_psi_feature_bin_proportions.sql");
+const GET_FEATURE_BIN_PROPORTIONS: &str =
+    include_str!("scripts/psi/get_feature_bin_proportions.sql");
+const GET_BIN_COUNT_ENTITIES: &str =
+    include_str!("scripts/psi/get_bin_count_entities_for_archive.sql");
+const GET_BIN_COUNT_DATA_FOR_ARCHIVE: &str =
+    include_str!("scripts/psi/get_bin_count_data_for_archive.sql");
+
+// custom
 const GET_BINNED_CUSTOM_METRIC_VALUES: &str =
-    include_str!("scripts/binned_custom_metric_values.sql");
-const GET_SPC_FEATURE_VALUES: &str = include_str!("scripts/get_spc_feature_values.sql");
+    include_str!("scripts/custom/binned_custom_metric_values.sql");
+const GET_CUSTOM_METRIC_VALUES: &str = include_str!("scripts/custom/get_custom_metric_values.sql");
+const INSERT_CUSTOM_METRIC_VALUES: &str =
+    include_str!("scripts/custom/insert_custom_metric_values.sql");
+const GET_CUSTOM_ENTITIES: &str =
+    include_str!("scripts/custom/get_custom_metric_entities_for_archive.sql");
+const GET_CUSTOM_DATA_FOR_ARCHIVE: &str =
+    include_str!("scripts/custom/get_custom_metric_data_for_archive.sql");
+
+// observability (experimental)
 const GET_BINNED_OBSERVABILITY_METRICS: &str =
-    include_str!("scripts/binned_observability_metrics.sql");
-const INSERT_DRIFT_PROFILE: &str = include_str!("scripts/insert_drift_profile.sql");
-const INSERT_DRIFT_ALERT: &str = include_str!("scripts/insert_drift_alert.sql");
-const INSERT_OBSERVABILITY_RECORD: &str = include_str!("scripts/insert_observability_record.sql");
-const GET_DRIFT_TASK: &str = include_str!("scripts/poll_for_drift_task.sql");
-const GET_DRIFT_ALERTS: &str = include_str!("scripts/get_drift_alerts.sql");
-const GET_DRIFT_PROFILE: &str = include_str!("scripts/get_drift_profile.sql");
+    include_str!("scripts/observability/binned_observability_metrics.sql");
+const INSERT_OBSERVABILITY_RECORD: &str =
+    include_str!("scripts/observability/insert_observability_record.sql");
+
+//profile
+const INSERT_DRIFT_PROFILE: &str = include_str!("scripts/profile/insert_drift_profile.sql");
+const GET_DRIFT_PROFILE: &str = include_str!("scripts/profile/get_drift_profile.sql");
 const UPDATE_DRIFT_PROFILE_RUN_DATES: &str =
-    include_str!("scripts/update_drift_profile_run_dates.sql");
-const UPDATE_DRIFT_PROFILE_STATUS: &str = include_str!("scripts/update_drift_profile_status.sql");
-const UPDATE_DRIFT_PROFILE: &str = include_str!("scripts/update_drift_profile.sql");
-const GET_FEATURE_BIN_PROPORTIONS: &str = include_str!("scripts/get_feature_bin_proportions.sql");
-const GET_CUSTOM_METRIC_VALUES: &str = include_str!("scripts/get_custom_metric_values.sql");
-const INSERT_CUSTOM_METRIC_VALUES: &str = include_str!("scripts/insert_custom_metric_values.sql");
-const GET_BIN_COUNT_ARCHIVE: &str = include_str!("scripts/get_bin_count_for_archive.sql");
-const GET_CUSTOM_ARCHIVE: &str = include_str!("scripts/get_custom_metric_for_archive.sql");
-const GET_SPC_ARCHIVE: &str = include_str!("scripts/get_spc_for_archive.sql");
+    include_str!("scripts/profile/update_drift_profile_run_dates.sql");
+const UPDATE_DRIFT_PROFILE_STATUS: &str =
+    include_str!("scripts/profile/update_drift_profile_status.sql");
+const UPDATE_DRIFT_PROFILE: &str = include_str!("scripts/profile/update_drift_profile.sql");
+
+// alert
+const INSERT_DRIFT_ALERT: &str = include_str!("scripts/alert/insert_drift_alert.sql");
+const GET_DRIFT_ALERTS: &str = include_str!("scripts/alert/get_drift_alerts.sql");
+
+// poll
+const GET_DRIFT_TASK: &str = include_str!("scripts/poll/poll_for_drift_task.sql");
 
 #[allow(dead_code)]
 pub enum Queries {
@@ -50,6 +75,9 @@ pub enum Queries {
     GetFeatureBinProportions,
     GetCustomMetricValues,
     InsertCustomMetricValues,
+    GeBinCountEntities,
+    GetCustomEntities,
+    GetSpcEntities,
     GetBinCountDataForArchive,
     GetCustomDataForArchive,
     GetSpcDataForArchive,
@@ -81,9 +109,12 @@ impl Queries {
             Queries::InsertBinCounts => SqlQuery::new(INSERT_BIN_COUNTS),
             Queries::GetCustomMetricValues => SqlQuery::new(GET_CUSTOM_METRIC_VALUES),
             Queries::InsertCustomMetricValues => SqlQuery::new(INSERT_CUSTOM_METRIC_VALUES),
-            Queries::GetBinCountDataForArchive => SqlQuery::new(GET_BIN_COUNT_ARCHIVE),
-            Queries::GetCustomDataForArchive => SqlQuery::new(GET_CUSTOM_ARCHIVE),
-            Queries::GetSpcDataForArchive => SqlQuery::new(GET_SPC_ARCHIVE),
+            Queries::GeBinCountEntities => SqlQuery::new(GET_BIN_COUNT_ENTITIES),
+            Queries::GetCustomEntities => SqlQuery::new(GET_CUSTOM_ENTITIES),
+            Queries::GetSpcEntities => SqlQuery::new(GET_SPC_ENTITIES),
+            Queries::GetBinCountDataForArchive => SqlQuery::new(GET_BIN_COUNT_DATA_FOR_ARCHIVE),
+            Queries::GetCustomDataForArchive => SqlQuery::new(GET_CUSTOM_DATA_FOR_ARCHIVE),
+            Queries::GetSpcDataForArchive => SqlQuery::new(GET_SPC_DATA_FOR_ARCHIVE),
         }
     }
 }
