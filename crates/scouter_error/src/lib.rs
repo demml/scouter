895c7da3db15b3c469c322b9f9af309df7484d0f
Thorrester <sjforrester32@gmail.com>
2025-04-20 07:20:37 -0400

    update util errors

--- crates/scouter_error/src/lib.rs
+++ crates/scouter_error/src/lib.rs
@@ -193,6 +193,9 @@ pub enum SqlError {
 
     #[error(transparent)]
     UtilError(#[from] UtilError),
+
+    #[error("Failed to extract value {0} {1}")]
+    FailedToExtractError(String, String),
 }
 
 impl TracedError for SqlError {}
@@ -260,6 +263,12 @@ impl SqlError {
 
     pub fn traced_get_drift_task_error(err: impl Display) -> Self {
         let error = Self::GetDriftTaskError(err.to_string());
+        error.trace();
+        error
+    }
+
+    pub fn traced_failed_to_extract_error(err: impl Display, field: impl Display) -> Self {
+        let error = Self::FailedToExtractError(field.to_string(), err.to_string());
         error.trace();
         error
     }
