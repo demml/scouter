e50c1400854542da284957d8461785f725fd0682
Thorrester <sjforrester32@gmail.com>
2025-04-22 14:01:09 -0400

    spc short and long data logic

--- crates/scouter_sql/src/sql/traits/spc.rs
+++ crates/scouter_sql/src/sql/traits/spc.rs
@@ -1,15 +1,21 @@
 use crate::sql::query::Queries;
 use crate::sql::schema::SpcFeatureResult;
+use crate::sql::utils::split_custom_interval;
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use scouter_contracts::{DriftRequest, ServiceInfo};
+use scouter_dataframe::parquet::{dataframe_to_spc_drift_features, ParquetDataFrame};
 use scouter_error::SqlError;
+use scouter_settings::ObjectStorageSettings;
 use scouter_types::{
     spc::{SpcDriftFeature, SpcDriftFeatures},
-    SpcServerRecord,
+    RecordType, SpcServerRecord,
 };
+
 use sqlx::{postgres::PgQueryResult, Pool, Postgres, Row};
 use std::collections::BTreeMap;
+use tracing::{debug, instrument};
+
 #[async_trait]
 pub trait SpcSqlLogic {
     /// Inserts a drift record into the database
@@ -107,24 +113,20 @@ pub trait SpcSqlLogic {
         })
     }
 
-    // Queries the database for drift records based on a time window and aggregation
-    //
-    // # Arguments
-    //
-    // * `name` - The name of the service to query drift records for
-    // * `space` - The name of the space to query drift records for
-    // * `feature` - The name of the feature to query drift records for
-    // * `aggregation` - The aggregation to use for the query
-    // * `time_interval` - The time window to query drift records for
-    //
-    // # Returns
-    //
-    // * A vector of drift records
-    async fn get_binned_spc_drift_records(
+    /// Queries the database for SPC drift records based on a time window
+    /// and aggregation.
+    ///
+    /// # Arguments
+    /// * `pool` - The database connection pool
+    /// * `params` - The drift request parameters
+    ///
+    /// # Returns
+    /// * SpcDriftFeatures
+    async fn get_records(
         pool: &Pool<Postgres>,
         params: &DriftRequest,
+        minutes: i32,
     ) -> Result<SpcDriftFeatures, SqlError> {
-        let minutes = params.time_interval.to_minutes();
         let bin = params.time_interval.to_minutes() as f64 / params.max_data_points as f64;
 
         let query = Queries::GetBinnedSpcFeatureValues.get_query();
@@ -153,5 +155,120 @@ pub trait SpcSqlLogic {
         Ok(SpcDriftFeatures {
             features: feature_drift,
         })
+    }
+
+    /// Helper for merging current and archived binned PSI drift records.
+    fn merge_feature_results(
+        results: SpcDriftFeatures,
+        map: &mut SpcDriftFeatures,
+    ) -> Result<(), SqlError> {
+        for (feature_name, feature) in results.features {
+            let feature_clone = feature.clone();
+            map.features
+                .entry(feature_name)
+                .and_modify(|existing| {
+                    existing.created_at.extend(feature_clone.created_at);
+                    existing.values.extend(feature_clone.values);
+                })
+                .or_insert(feature);
+        }
+
+        Ok(())
+    }
+
+    /// DataFusion implementation for getting PSI drift records from archived data.
+    ///
+    /// # Arguments
+    /// * `params` - The drift request parameters
+    /// * `begin` - The start time of the time window
+    /// * `end` - The end time of the time window
+    /// * `minutes` - The number of minutes to bin the data
+    /// * `storage_settings` - The object storage settings
+    ///
+    /// # Returns
+    /// * A vector of drift records
+    async fn get_archived_records(
+        params: &DriftRequest,
+        begin: DateTime<Utc>,
+        end: DateTime<Utc>,
+        minutes: i32,
+        storage_settings: &ObjectStorageSettings,
+    ) -> Result<SpcDriftFeatures, SqlError> {
+        let path = format!("{}/{}/{}/psi", params.space, params.name, params.version);
+        let bin = minutes as f64 / params.max_data_points as f64;
+
+        let archived_df = ParquetDataFrame::new(storage_settings, &RecordType::Spc)?
+            .get_binned_metrics(
+                &path,
+                &bin,
+                &begin,
+                &end,
+                &params.space,
+                &params.name,
+                &params.version,
+            )
+            .await?;
+
+        dataframe_to_spc_drift_features(archived_df)
+            .await
+            .map_err(SqlError::traced_failed_to_convert_dataframe_error)
+    }
+
+    // Queries the database for drift records based on a time window and aggregation
+    //
+    // # Arguments
+    //
+    // * `name` - The name of the service to query drift records for
+    // * `space` - The name of the space to query drift records for
+    // * `feature` - The name of the feature to query drift records for
+    // * `aggregation` - The aggregation to use for the query
+    // * `time_interval` - The time window to query drift records for
+    //
+    // # Returns
+    //
+    // * A vector of drift records
+    #[instrument(skip_all)]
+    async fn get_binned_spc_drift_records(
+        pool: &Pool<Postgres>,
+        params: &DriftRequest,
+        retention_period: &i32,
+        storage_settings: &ObjectStorageSettings,
+    ) -> Result<SpcDriftFeatures, SqlError> {
+        debug!("Getting binned SPC drift records for {:?}", params);
+
+        if !params.has_custom_interval() {
+            debug!("No custom interval provided, using default");
+            let minutes = params.time_interval.to_minutes();
+            return Self::get_records(pool, params, minutes).await;
+        }
+
+        debug!("Custom interval provided, using custom interval");
+        let interval = params.clone().to_custom_interval().unwrap();
+        let timestamps = split_custom_interval(interval.start, interval.end, retention_period)?;
+        let mut spc_feature_map = SpcDriftFeatures::default();
+
+        if let Some(minutes) = timestamps.current_minutes {
+            let current_results = Self::get_records(pool, params, minutes).await?;
+
+            debug!("Current results: {:?}", current_results);
+            Self::merge_feature_results(current_results, &mut spc_feature_map)?;
+        }
+
+        if let Some((archive_begin, archive_end)) = timestamps.archived_range {
+            if let Some(archived_minutes) = timestamps.archived_minutes {
+                let archived_results = Self::get_archived_records(
+                    params,
+                    archive_begin,
+                    archive_end,
+                    archived_minutes,
+                    storage_settings,
+                )
+                .await?;
+
+                Self::merge_feature_results(archived_results, &mut spc_feature_map)?;
+            }
+        }
+
+        Ok(spc_feature_map)
     }
 }
