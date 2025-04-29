826ec468e431494975008fcddc14ad2f9513dc18
Thorrester <sjforrester32@gmail.com>
2025-04-29 09:23:07 -0400

    setting up producer

--- crates/scouter_events/src/producer/redis/producer.rs
+++ crates/scouter_events/src/producer/redis/producer.rs
@@ -1,9 +1,12 @@
 #[cfg(feature = "redis_events")]
 pub mod redis_producer {
-    use redis::aio::PubSub;
-    use redis::{Client, Msg, RedisResult};
+    use redis::aio::{MultiplexedConnection, PubSub};
+    use redis::AsyncCommands;
+    use redis::Client;
     use scouter_error::EventError;
-
+    use scouter_settings::RedisSettings;
+    use scouter_types::ServerRecords;
+    use tracing::debug;
     pub struct RedisMessageBroker {
         client: Client,
     }
@@ -15,11 +18,86 @@ pub mod redis_producer {
             Ok(Self { client })
         }
 
+        pub async fn get_connection(&self) -> Result<MultiplexedConnection, EventError> {
+            self.client
+                .get_multiplexed_async_connection()
+                .await
+                .map_err(|e| EventError::RedisConnectionError(e.to_string()))
+        }
+
         pub async fn get_pub_sub(&self) -> Result<PubSub, EventError> {
             self.client
                 .get_async_pubsub()
                 .await
                 .map_err(|e| EventError::RedisPubSubError(e.to_string()))
+        }
+    }
+
+    pub struct RedisProducer {
+        pub connection: MultiplexedConnection,
+        pub channel: String,
+        pub max_retries: usize,
+    }
+
+    impl RedisProducer {
+        /// Create a new Redis producer
+        /// This function creates a new Redis producer that will publish messages to the specified channel
+        ///
+        /// # Arguments
+        /// * `config` - The Redis settings
+        ///
+        /// # Returns
+        /// * `Result<RedisProducer, EventError>` - The result of the operation
+        pub async fn new(config: RedisSettings) -> Result<Self, EventError> {
+            let broker = RedisMessageBroker::new(&config.address)?;
+            Ok(Self {
+                connection: broker.get_connection().await?,
+                channel: config.channel.clone(),
+                max_retries: 3,
+            })
+        }
+
+        /// Core publish method used to publish messages to Redis
+        /// This function publishes messages to Redis. It will retry the specified number of times if the publish fails
+        ///
+        /// # Arguments
+        /// * `message` - The message to publish
+        ///
+        /// # Returns
+        /// * `Result<(), EventError>` - The result of the operation
+        pub async fn publish(&mut self, message: ServerRecords) -> Result<(), EventError> {
+            let mut retries = self.max_retries;
+
+            loop {
+                match self._publish(message.clone()).await {
+                    Ok(_) => break,
+                    Err(e) => {
+                        retries -= 1;
+                        if retries == 0 {
+                            return Err(e);
+                        }
+                    }
+                }
+            }
+
+            Ok(())
+        }
+
+        /// Async publish to Redis
+        pub async fn _publish(&mut self, message: ServerRecords) -> Result<(), EventError> {
+            let serialized_msg = serde_json::to_string(&message).unwrap().into_bytes();
+
+            debug!("Publishing message to Redis");
+            let _: () = self
+                .connection
+                .publish(&self.channel, &serialized_msg)
+                .await
+                .map_err(EventError::traced_publish_error)?;
+            Ok(())
+        }
+
+        pub async fn flush(&self) -> Result<(), EventError> {
+            Ok(())
         }
     }
 }
