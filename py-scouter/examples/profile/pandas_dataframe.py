e6d237eb8d7c0eb4645e307b1e91f827fa5f1688
Thorrester <sjforrester32@gmail.com>
2025-04-29 13:29:58 -0400

    Update pandas_dataframe.py

--- py-scouter/examples/profile/pandas_dataframe.py
+++ py-scouter/examples/profile/pandas_dataframe.py
@@ -6,18 +6,18 @@ from scouter import DataProfile, DataProfiler  # type: ignore[attr-defined]
 def generate_data() -> pd.DataFrame:
     """Create a fake data frame for testing"""
     n = 10_000
-    X_train = np.random.normal(-4, 2.0, size=(n, 4))
+    x_train = np.random.normal(-4, 2.0, size=(n, 4))
     col_names = []
-    for i in range(0, X_train.shape[1]):
+    for i in range(0, x_train.shape[1]):
         col_names.append(f"feature_{i}")
-    X = pd.DataFrame(X_train, columns=col_names)
+    x = pd.DataFrame(x_train, columns=col_names)
 
     # create string column (with 10 unique values)
-    X["categorical_feature"] = np.random.choice(
+    x["categorical_feature"] = np.random.choice(
         ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], size=n
     )
 
-    return X
+    return x
 
 
 data = generate_data()
