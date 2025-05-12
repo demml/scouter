fba8daea21e2304e6c4a0ec8e773e157e95d44aa
Thorrester <sjforrester32@gmail.com>
2025-05-12 09:57:40 -0400

    Update psi.rs

--- crates/scouter_sql/src/sql/traits/psi.rs
+++ crates/scouter_sql/src/sql/traits/psi.rs
@@ -6,7 +6,7 @@ use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use scouter_dataframe::parquet::{dataframe_to_psi_drift_features, ParquetDataFrame};
 
-use scouter_error::SqlError;
+use crate::sql::error::SqlError;
 use scouter_settings::ObjectStorageSettings;
 use scouter_types::{
     psi::{FeatureBinProportionResult, FeatureBinProportions},
@@ -42,7 +42,7 @@ pub trait PsiSqlLogic {
             .bind(record.bin_count as i64)
             .execute(pool)
             .await
-            .map_err(SqlError::traced_query_error)
+            .map_err(SqlError::SqlxError)
     }
 
     /// Queries the database for PSI drift records based on a time window
@@ -70,7 +70,7 @@ pub trait PsiSqlLogic {
             .bind(&params.version)
             .fetch_all(pool)
             .await
-            .map_err(SqlError::traced_query_error)?
+            .map_err(SqlError::SqlxError)?
             .into_iter()
             .map(|wrapper: FeatureBinProportionResultWrapper| wrapper.0)
             .collect();
@@ -112,9 +112,7 @@ pub trait PsiSqlLogic {
             )
             .await?;
 
-        dataframe_to_psi_drift_features(archived_df)
-            .await
-            .map_err(SqlError::traced_failed_to_convert_dataframe_error)
+        Ok(dataframe_to_psi_drift_features(archived_df).await?)
     }
 
     /// Helper for merging current and archived binned PSI drift records.
@@ -217,7 +215,7 @@ pub trait PsiSqlLogic {
             .bind(features_to_monitor)
             .fetch_all(pool)
             .await
-            .map_err(SqlError::traced_get_bin_proportions_error)?;
+            .map_err(SqlError::SqlxError)?;
 
         let binned: FeatureBinProportions = binned.into_iter().map(|wrapper| wrapper.0).collect();
 
