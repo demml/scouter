3107070a64f90adac67e897a70b24bae697fe358
Thorrester <sjforrester32@gmail.com>
2025-05-16 15:25:14 -0400

    format

--- py-scouter/tests/test_profile/test_profile.py
+++ py-scouter/tests/test_profile/test_profile.py
@@ -14,15 +14,9 @@ def test_data_profile_f64(array: NDArray):
 
     # assert features are relatively centered
     assert profile.features["feature_0"].numeric_stats.mean == pytest.approx(1.5, 0.1)
-    assert profile.features["feature_0"].numeric_stats.distinct.count == pytest.approx(
-        1000, 1
-    )
-    assert profile.features["feature_0"].numeric_stats.quantiles.q25 == pytest.approx(
-        1.25, 0.1
-    )
-    assert profile.features["feature_0"].numeric_stats.histogram.bins[
-        0
-    ] == pytest.approx(1.00, 0.1)
+    assert profile.features["feature_0"].numeric_stats.distinct.count == pytest.approx(1000, 1)
+    assert profile.features["feature_0"].numeric_stats.quantiles.q25 == pytest.approx(1.25, 0.1)
+    assert profile.features["feature_0"].numeric_stats.histogram.bins[0] == pytest.approx(1.00, 0.1)
     assert len(profile.features["feature_0"].numeric_stats.histogram.bin_counts) == 20
 
     # convert to json
@@ -31,9 +25,7 @@ def test_data_profile_f64(array: NDArray):
     # load from json
     loaded_profile = DataProfile.model_validate_json(json_str)
 
-    assert loaded_profile.features["feature_0"].numeric_stats.mean == pytest.approx(
-        1.5, 0.1
-    )
+    assert loaded_profile.features["feature_0"].numeric_stats.mean == pytest.approx(1.5, 0.1)
 
     # save to json
     loaded_profile.save_to_json(Path("assets/data_profile.json"))
@@ -56,15 +48,9 @@ def test_data_profile_f32(array: NDArray):
 
     # assert features are relatively centered
     assert profile.features["feature_0"].numeric_stats.mean == pytest.approx(1.5, 0.1)
-    assert profile.features["feature_0"].numeric_stats.distinct.count == pytest.approx(
-        1000, 1
-    )
-    assert profile.features["feature_0"].numeric_stats.quantiles.q25 == pytest.approx(
-        1.25, 0.1
-    )
-    assert profile.features["feature_0"].numeric_stats.histogram.bins[
-        0
-    ] == pytest.approx(1.00, 0.1)
+    assert profile.features["feature_0"].numeric_stats.distinct.count == pytest.approx(1000, 1)
+    assert profile.features["feature_0"].numeric_stats.quantiles.q25 == pytest.approx(1.25, 0.1)
+    assert profile.features["feature_0"].numeric_stats.histogram.bins[0] == pytest.approx(1.00, 0.1)
     assert len(profile.features["feature_0"].numeric_stats.histogram.bin_counts) == 20
 
 
@@ -75,15 +61,9 @@ def test_data_profile_polars(array: NDArray):
 
     # assert features are relatively centered
     assert profile.features["column_0"].numeric_stats.mean == pytest.approx(1.5, 0.1)
-    assert profile.features["column_0"].numeric_stats.distinct.count == pytest.approx(
-        1000, 1
-    )
-    assert profile.features["column_0"].numeric_stats.quantiles.q25 == pytest.approx(
-        1.25, 0.1
-    )
-    assert profile.features["column_0"].numeric_stats.histogram.bins[
-        0
-    ] == pytest.approx(1.00, 0.1)
+    assert profile.features["column_0"].numeric_stats.distinct.count == pytest.approx(1000, 1)
+    assert profile.features["column_0"].numeric_stats.quantiles.q25 == pytest.approx(1.25, 0.1)
+    assert profile.features["column_0"].numeric_stats.histogram.bins[0] == pytest.approx(1.00, 0.1)
     assert len(profile.features["column_0"].numeric_stats.histogram.bin_counts) == 20
 
 
@@ -103,9 +83,7 @@ def test_data_profile_pandas(array: NDArray):
     assert profile.features["0"].numeric_stats.mean == pytest.approx(1.5, 0.1)
     assert profile.features["0"].numeric_stats.distinct.count == pytest.approx(1000, 1)
     assert profile.features["0"].numeric_stats.quantiles.q25 == pytest.approx(1.25, 0.1)
-    assert profile.features["0"].numeric_stats.histogram.bins[0] == pytest.approx(
-        1.00, 0.1
-    )
+    assert profile.features["0"].numeric_stats.histogram.bins[0] == pytest.approx(1.00, 0.1)
     assert len(profile.features["0"].numeric_stats.histogram.bin_counts) == 20
 
 
@@ -116,33 +94,23 @@ def test_data_profile_polars_mixed_type(
     profile: DataProfile = scouter.create_data_profile(polars_dataframe_multi_dtype)
 
     assert profile.features["cat2"].string_stats.distinct.count == 3
-    assert profile.features["cat2"].string_stats.word_stats.words[
-        "3.0"
-    ].percent == pytest.approx(0.352, abs=0.1)
+    assert profile.features["cat2"].string_stats.word_stats.words["3.0"].percent == pytest.approx(0.352, abs=0.1)
 
     assert profile.features["cat1"].string_stats.distinct.count == 5
-    assert profile.features["cat1"].string_stats.word_stats.words[
-        "7.0"
-    ].percent == pytest.approx(0.19, abs=0.1)
+    assert profile.features["cat1"].string_stats.word_stats.words["7.0"].percent == pytest.approx(0.19, abs=0.1)
 
 
 def test_data_profile_pandas_mixed_type(
     polars_dataframe_multi_dtype: pl.DataFrame,
 ):
     scouter = DataProfiler()
-    profile: DataProfile = scouter.create_data_profile(
-        polars_dataframe_multi_dtype.to_pandas()
-    )
+    profile: DataProfile = scouter.create_data_profile(polars_dataframe_multi_dtype.to_pandas())
 
     assert profile.features["cat2"].string_stats.distinct.count == 3
-    assert profile.features["cat2"].string_stats.word_stats.words[
-        "3.0"
-    ].percent == pytest.approx(0.352, abs=0.1)
+    assert profile.features["cat2"].string_stats.word_stats.words["3.0"].percent == pytest.approx(0.352, abs=0.1)
 
     assert profile.features["cat1"].string_stats.distinct.count == 5
-    assert profile.features["cat1"].string_stats.word_stats.words[
-        "7.0"
-    ].percent == pytest.approx(0.19, abs=0.1)
+    assert profile.features["cat1"].string_stats.word_stats.words["7.0"].percent == pytest.approx(0.19, abs=0.1)
 
 
 def test_data_profile_pyarrow_mixed_type(
@@ -154,11 +122,7 @@ def test_data_profile_pyarrow_mixed_type(
     profile: DataProfile = scouter.create_data_profile(arrow_table)
 
     assert profile.features["cat2"].string_stats.distinct.count == 3
-    assert profile.features["cat2"].string_stats.word_stats.words[
-        "3.0"
-    ].percent == pytest.approx(0.352, abs=0.1)
+    assert profile.features["cat2"].string_stats.word_stats.words["3.0"].percent == pytest.approx(0.352, abs=0.1)
 
     assert profile.features["cat1"].string_stats.distinct.count == 5
-    assert profile.features["cat1"].string_stats.word_stats.words[
-        "7.0"
-    ].percent == pytest.approx(0.19, abs=0.1)
+    assert profile.features["cat1"].string_stats.word_stats.words["7.0"].percent == pytest.approx(0.19, abs=0.1)
