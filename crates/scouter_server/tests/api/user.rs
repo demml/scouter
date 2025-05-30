1af789473b59c0d90855488597ea4de0e9076cf1
Thorrester <sjforrester32@gmail.com>
2025-05-30 16:42:04 -0400

    Update user.rs

--- crates/scouter_server/tests/api/user.rs
+++ crates/scouter_server/tests/api/user.rs
@@ -86,7 +86,9 @@ async fn test_server_user_crud() {
     assert_eq!(response.status(), StatusCode::OK);
 
     let body = response.into_body().collect().await.unwrap().to_bytes();
-    let user_response: UserResponse = serde_json::from_slice(&body).unwrap();
+    let mut user_response: UserResponse = serde_json::from_slice(&body).unwrap();
+    user_response.permissions.sort();
+
     assert_eq!(
         user_response.permissions,
         vec![
