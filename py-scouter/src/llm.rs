82d16f5268544e80eb38eb95e8584807d1815434
Thorrester <sjforrester32@gmail.com>
2025-07-17 13:43:46 -0400

    bump

--- py-scouter/src/llm.rs
+++ py-scouter/src/llm.rs
@@ -1,6 +1,7 @@
 use potato_head::{
-    AudioUrl, BinaryContent, DocumentUrl, ImageUrl, Message, ModelSettings, Prompt, Provider,
-    PyAgent, PyWorkflow, Score, Task,
+    AudioUrl, BinaryContent, ChatResponse, DocumentUrl, EventDetails, ImageUrl, Message,
+    ModelSettings, Prompt, Provider, PyAgent, PyTask, PyWorkflow, Score, Task, TaskEvent,
+    WorkflowResult,
 };
 use pyo3::prelude::*;
 #[pymodule]
@@ -9,6 +10,7 @@ pub fn llm(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyAgent>()?;
     m.add_class::<PyWorkflow>()?;
     m.add_class::<Task>()?;
+    m.add_class::<PyTask>()?;
     m.add_class::<Prompt>()?;
     m.add_class::<Message>()?;
     m.add_class::<ModelSettings>()?;
@@ -17,5 +19,9 @@ pub fn llm(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<BinaryContent>()?;
     m.add_class::<DocumentUrl>()?;
     m.add_class::<ImageUrl>()?;
+    m.add_class::<EventDetails>()?;
+    m.add_class::<ChatResponse>()?;
+    m.add_class::<TaskEvent>()?;
+    m.add_class::<WorkflowResult>()?;
     Ok(())
 }
