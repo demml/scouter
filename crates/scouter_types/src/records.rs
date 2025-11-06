bada5e00156a4dcb098263db8323bb6a08fa4024
Thorrester <sjforrester32@gmail.com>
2025-11-06 09:38:41 -0500

    adding parent enum for consumer messages

--- crates/scouter_types/src/records.rs
+++ crates/scouter_types/src/records.rs
@@ -692,3 +692,84 @@ fn extract_records<T>(
 
     Ok(records)
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct TraceRecord {
+    pub trace_id: String,
+    pub space: String,
+    pub name: String,
+    pub version: String,
+    pub drift_type: String,
+    pub service_name: String,
+    pub trace_state: String,
+    pub start_time: chrono::DateTime<Utc>,
+    pub end_time: chrono::DateTime<Utc>,
+    pub duration_ms: i64,
+    pub status: String,
+    pub root_span_id: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct TraceSpanRecord {
+    pub span_id: String,
+    pub trace_id: String,
+    pub parent_span_id: String,
+    pub space: String,
+    pub name: String,
+    pub version: String,
+    pub drift_type: String,
+    pub service_name: String,
+    pub operation_name: String,
+    pub span_kind: String,
+    pub start_time: chrono::DateTime<Utc>,
+    pub end_time: chrono::DateTime<Utc>,
+    pub duration_ms: i64,
+    pub status_code: String,
+    pub status_message: String,
+    pub attributes: Value,
+    pub events: Value,
+    pub links: Value,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct TraceBaggageRecord {
+    pub trace_id: String,
+    pub service_name: String,
+    pub key: String,
+    pub value: String,
+    pub space: String,
+    pub name: String,
+    pub version: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct TraceServerRecord {
+    pub request: ExportTraceServiceRequest,
+}
+
+impl RecordExt for TraceServerRecord {
+    fn record_type(&self) -> Result<RecordType, RecordError> {
+        Ok(RecordType::Trace)
+    }
+}
+
+pub enum MessageType {
+    Server,
+    Trace,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum MessageRecord {
+    ServerRecords(ServerRecords),
+    TraceServerRecord(TraceServerRecord),
+}
+
+impl MessageRecord {
+    pub fn record_type(&self) -> MessageType {
+        match self {
+            MessageRecord::ServerRecords(_) => MessageType::Server,
+            MessageRecord::TraceServerRecord(_) => MessageType::Trace,
+        }
+    }
+}
